92d7e99431e4aa37d7e6f51d0d930d34f0d408b0
Greg Chlosta <you@example.com>
2021-11-13 19:08:04 -0700

    Header

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Instagram Clone</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Header />
+
+      <h1>Instagram clone</h1>
+    </div>
+  )
+}
