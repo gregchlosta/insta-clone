95f71cf239fd80289db5650410d3f9a07dd5c4a5
Greg Chlosta <grzegorz.chlosta@gmail.com>
2021-12-28 13:23:30 -0700

    pulling post from firestore

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,35 +1,32 @@
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
 import Post from './Post'
 
-const POSTS = [
-  {
-    id: '1',
-    username: 'greg',
-    userImg:
-      'https://lh3.googleusercontent.com/ogw/ADea4I76Q-SDgTSHa-wldtLHkCmsu9rH7Qh4uhGjKnrZdA=s83-c-mo',
-    img: 'https://images.cdn.circlesix.co/image/1/700/0/uploads/media/2020-02/14/d363e6c380150fe0/img_1138_ca.jpg',
-    caption: 'Caption text',
-  },
-  {
-    id: '2',
-    username: 'greg',
-    userImg:
-      'https://lh3.googleusercontent.com/ogw/ADea4I76Q-SDgTSHa-wldtLHkCmsu9rH7Qh4uhGjKnrZdA=s83-c-mo',
-    img: 'https://images.cdn.circlesix.co/image/1/700/0/uploads/media/2020-02/14/d363e6c380150fe0/img_1138_ca.jpg',
-    caption: 'Caption text',
-  },
-]
-
 export default function Posts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+      (snapshot) => {
+        setPosts(snapshot.docs)
+      }
+    )
+
+    return unsubscribe
+  }, [db])
+
   return (
     <div>
-      {POSTS.map((post) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
